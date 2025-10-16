c13c73642e6fab4995464edde4eee1b09eaf3dad
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-10-16 19:22:27 +0100

    Creating project

---
+++ src/main/java/ie/atu/lab3cicddp/Lab3CicdDpApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3cicddp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3CicdDpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3CicdDpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab3cicddp/Lab3CicdDpApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3cicddp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3CicdDpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
